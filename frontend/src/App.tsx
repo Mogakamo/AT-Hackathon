971536fed2ab5ddbfb6f64d7a9a69cbe81095966
Mogakamo <mogaka.amo254@gmail.com>
2022-06-29 21:35:15 +0300

    Added the routing option for the client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
-
-  return <div className="">HEllo World</div>;
+  return (
+    <div className="">
+      <Link to="/about">About</Link>
+    </div>
+  );
 }
 
 export default App;
