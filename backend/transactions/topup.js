e55e91e4919caeccd52ae451c5939dcd4e93835c
Mogakamo <mogaka.amo254@gmail.com>
2022-06-30 04:54:45 +0300

    client: Login and register

--- backend/transactions/topup.js
+++ backend/transactions/topup.js
@@ -1,5 +1,24 @@
-import {credentials} from "../config/credentials"
+const credentials = require("../config/credentials");
 
+const AfricasTalking = require("africastalking")(credentials);
 
-const AfricasTalking = require('africastalking')(credentials);
+const payments = AfricasTalking.PAYMENTS;
 
+async function initiateStash() {
+  const options = {
+    productName: "Elimisha",
+    currencyCode: "KES",
+    amount: 1000,
+    metadata: {
+      Metadata: "This is a test",
+    },
+  };
+  try {
+    const result = await payments.topupStash(options);
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+initiateStash();
