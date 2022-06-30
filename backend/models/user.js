84a12de448dede1b2290439c0e7802bc62793b47
Mogakamo <mogaka.amo254@gmail.com>
2022-06-30 05:23:33 +0300

    encrypting the password

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-let userSchema = new Schema(
+// Roles
+const roles = ["admin", "user"];
+
+/**
+ * User Schema
+ * @private
+ */
+const userSchema = new Schema(
   {
     name: {
       type: String,
@@ -9,21 +16,46 @@ let userSchema = new Schema(
     },
     email: {
       type: String,
+      match: /^\S+@\S+\.\S+$/,
       required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
     },
     password: {
       type: String,
       required: true,
+      minlength: 6,
+      maxlength: 128,
     },
-    added_on: {
-      type: Date,
-      default: Date.now,
+    role: {
+      type: String,
+      enum: roles,
+      default: "user",
+    },
+    balance: {
+      type: Number,
+      default: 0,
+      min: 0,
     },
   },
+  { timestamps: true },
   {
     collection: "users",
   }
 );
 
+
+// Hash the password before saving
+userSchema.pre("save",  save = async(next) => {
+  try {
+    if(!this.isModified('password')) return next();
+
+    const rounds = env === 'test' ? 1 : 10;
+
+    // const hash = 
+  } catch (err) {
+    return next(error)
+  }
 
 module.exports = mongoose.model("User", userSchema);
